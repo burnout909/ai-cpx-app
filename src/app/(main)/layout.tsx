7f5f03d4f0d1e04834551b27a1e1eafbef8dd26b
burnout909 <znlwmdhkd1@yonsei.ac.kr>
2025-11-10 21:02:24 +0900

    edit header layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <div className="flex flex-col min-h-dvh">
-            <Header />
             {children}
         </div>
     )
